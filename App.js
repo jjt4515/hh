8f4dae9d2a1799b35f139f6e1a06faf7cb5ac68d
jjt4515 <koreantaxi@naver.com>
2022-03-14 15:57:48 +0000

    App.js

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
+import { AdMobInterstitial } from 'expo-ads-admob';
+
+
 export default class App extends React.Component {
-  render() {
-    return <WebView source={{ uri: 'https://fashionstyletest.ga/' }} style={{ marginTop: 20 }} />;
-  }
-}
+	adFunction = async () => {
+		await AdMobInterstitial.setAdUnitID('ca-app-pub-1870519677513892/7223294991'); 
+		await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
+		await AdMobInterstitial.showAdAsync();
+	}
+	
+	componentDidMount() {
+		this.adFunction();
+	}
+	
+    render() {
+   		return <WebView source={{ uri: 'https://fashionstyletest.ga/' }}/>;
+	}
+}	
+
---
+++ README.md
@@ -0,0 +1,3 @@
+## this project..
+
+이 프로젝트에서는 'styleproject2-fashionstyle'에서 구현한 웹사이트를 webview 방식의 앱으로 제작하였습니다.
