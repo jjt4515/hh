8b515c9f008157ceb4590e13dd92182523bccabf
정진택 <87135698+jjt4515@users.noreply.github.com>
2022-03-15 00:59:23 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
-import { AdMobInterstitial } from 'expo-ads-admob';
 
 
 export default class App extends React.Component {
-	adFunction = async () => {
-		await AdMobInterstitial.setAdUnitID('ca-app-pub-1870519677513892/7223294991'); 
-		await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
-		await AdMobInterstitial.showAdAsync();
-	}
-	
-	componentDidMount() {
-		this.adFunction();
-	}
-	
     render() {
    		return <WebView source={{ uri: 'https://fashionstyletest.ga/' }}/>;
 	}
